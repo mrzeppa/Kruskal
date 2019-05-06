ebcf5db9f9911d46249da613e61da3c1a15d2ba1
mrzeppa <mrzeppa16@gmail.com>
2019-05-06 19:26:40 +0200

    final changes

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,22 @@ public class Main {
 
 
 
-        Graph g = new Graph(4, 5);
-        g.addEdge(new Edge(0, 1, 10));
-        g.addEdge(new Edge(0, 2, 6));
-        g.addEdge(new Edge(0, 3, 5));
-        g.addEdge(new Edge(1, 3, 15));
-        g.addEdge(new Edge(2, 3, 4));
+        Graph g = new Graph(9, 15); // tu +1 do il. wezlow
+        g.addEdge(new Edge(3, 5, 2));
+        g.addEdge(new Edge(6, 7, 5));
+        g.addEdge(new Edge(3, 4, 6));
+        g.addEdge(new Edge(4, 8, 7));
+        g.addEdge(new Edge(1, 2, 9));
+        g.addEdge(new Edge(4, 5, 11));
+        g.addEdge(new Edge(1, 6, 14));
+        g.addEdge(new Edge(1, 7, 15));
+        g.addEdge(new Edge(5, 8, 16));
+        g.addEdge(new Edge(3, 6, 18));
+        g.addEdge(new Edge(3, 8, 19));
+        g.addEdge(new Edge(7, 5, 20));
+        g.addEdge(new Edge(2, 3, 24));
+        g.addEdge(new Edge(7, 8, 44));
+        g.addEdge(new Edge(6, 5, 30));
         g.kruskal();
     }
 }
